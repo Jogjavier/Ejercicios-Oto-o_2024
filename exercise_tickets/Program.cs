a9d6028eca1d3928bb486d5bdfa06bd77e9cdb27
jeserra <joser_isi@hotmail.com>
2024-10-30 18:36:32 -0600

    add functionality of save

--- exercise_tickets/Program.cs
+++ exercise_tickets/Program.cs
@@ -8,6 +8,13 @@
 /* Crear un metodo que, reciba como parametro el id del boleto y cambie el status a vendido y muestre los boletos comprados por el usuario
 comprador */
 
+/* Crear un metodo para guardar la informacion de los tickets para vender */
+
+
+/* Obtener el listado de tickets disponibles desde este archivo cuando un comprador pregunte por ellos */
+
+using exercise_tickets;
+
 Console.WriteLine("Ingresar datos del usuario");
 
 
@@ -27,7 +34,21 @@ if(input == 'V')
 
 }
 else if (input == 'C')
+{
+
     usuario = new Comprador();
+    TicketForms ticketForms = new TicketForms(new Vendedor());
+    ticketForms.MostrarBoletosDisponibles();
+    Console.ReadKey();
+    
+    CompradorForms compradorForms = new CompradorForms((Comprador)usuario);
+    Console.WriteLine("Ingresa el id del ticket que quieres comprar?");
+    string strTicket = Console.ReadLine();
+    int idTicket = Int32.Parse(strTicket);
+    
+    compradorForms.ComprarBoleto(idTicket, ticketForms._listaTickets);
+     
+}
 else 
     return;
 
