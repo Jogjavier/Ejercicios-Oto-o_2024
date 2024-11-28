a2b077b717ec665a9a842086d0563cf53d10f424
jeserra <joser_isi@hotmail.com>
2024-11-27 18:29:02 -0600

    added method to interfaz and class to implement json

---
+++ exercise_tickets/JsonFileRepository.cs
@@ -0,0 +1,15 @@
+
+// Implementar metodo de guardado de archivo json en vez de .txt
+// Con Serializacion y deserializacion
+public class JsonFileRepository : IFileRepository
+{
+    public string SaveFile(string fileName, string Content)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string SaveFile(Vendedor vendedor)
+    {
+        throw new NotImplementedException();
+    }
+}
