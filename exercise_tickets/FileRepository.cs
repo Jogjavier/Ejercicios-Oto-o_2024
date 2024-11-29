8e615fc852e05ae012e2a7ec45a733eeea35c45e
Shiroko <ivanpalacios2809@gmail.com>
2024-11-29 16:34:47 -0600

    Add SaveJsonInFile method to FileRepository for JSON file handling

--- exercise_tickets/FileRepository.cs
+++ exercise_tickets/FileRepository.cs
@@ -2,7 +2,7 @@ public class FileRepository : IFileRepository
 {
     public string SaveFile(string fileName, string content)
     {
-        try 
+        try
         {
             File.AppendAllText(fileName, content);
             return "Archivo guardado exitosamente.";
@@ -10,6 +10,19 @@ public class FileRepository : IFileRepository
         catch (Exception ex)
         {
             return $"Error al guardar el archivo: {ex.Message}";
+        }
+    }
+    public string SaveJsonInFile<T>(string fileName, T obj)
+    {
+        try
+        {
+            string jsonString = JsonSerializer.Serialize(obj);
+            File.WriteAllText(fileName, jsonString);
+            return "Archivo JSON guardado exitosamente.";
+        }
+        catch (Exception ex)
+        {
+            return $"Error al guardar el archivo JSON: {ex.Message}";
         }
     }
 }
